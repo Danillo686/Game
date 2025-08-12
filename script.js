2adc5bc30eb5bb150bfd717893525361b7ba6fc9
Danillo686 <uqualquer220@gmail.com>
2025-08-12 16:33:28 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let confings = document.getElementById("BotãoSetting");
+let day = document.getElementById("Botão");
+let body = document.getElementById("Corpo")
+
+function NightMode() {
+    body.classList.add("Night") 
+}
+
